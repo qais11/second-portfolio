f87ca34f7dcea9823aeb4abb7447e61684d81339
Samer <samaladhami@gmail.com>
2017-05-20 00:18:34 -0500

    setting up canvas

---
+++ app.js
@@ -0,0 +1,14 @@
+//Create the renderer
+let renderer = PIXI.autoDetectRenderer( 500 , 500 );
+renderer.view.style.position = "absolute";
+renderer.view.style.display = "block";
+renderer.autoResize = true;
+renderer.resize(window.innerWidth, window.innerHeight);
+
+//Add the canvas to the HTML document
+document.body.appendChild(renderer.view);
+
+//Create a container object called the `stage`
+var stage = new PIXI.Container();
+//Tell the `renderer` to `render` the `stage`
+renderer.render(stage);
