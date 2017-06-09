13be2682a4802b0300835d255d430927794e5936
qais <qais.gh11@gmail.com>
2017-06-08 22:26:14 -0500

    Adding stars to the background

--- app.js
+++ app.js
@@ -11,4 +11,21 @@ document.body.appendChild(renderer.view);
 //Create a container object called the `stage`
 var stage = new PIXI.Container();
 //Tell the `renderer` to `render` the `stage`
+
+function drawStars(){
+  for(var i = 0 ; i < 1500 ; i++){
+    var x = Math.round(Math.random() * window.innerWidth)
+    var y = Math.round(Math.random() * window.innerHeight)
+    var rad = Math.ceil(Math.random() * 2)
+    var alpha = Math.min(Math.random() +  0.25 , 1)
+
+    var star = new PIXI.Graphics();
+    star.beginFill( 0xFFFFFF , alpha)
+    star.drawCircle(x , y , rad)
+    star.endFill()
+    stage.addChild(star)
+
+  }
+}
+drawStars()
 renderer.render(stage);
