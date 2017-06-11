4d3b8c8f5d4d636770898a967ae7e6722cdc38ba
qais <qais.gh11@gmail.com>
2017-06-11 01:43:35 -0500

    adding & creating sprites & animations

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Create the renderer
-let renderer = PIXI.autoDetectRenderer( 500 , 500 );
+var renderer = PIXI.autoDetectRenderer( 1000 , 1000 );
 renderer.view.style.position = "absolute";
 renderer.view.style.display = "block";
 renderer.autoResize = true;
@@ -10,22 +10,128 @@ document.body.appendChild(renderer.view);
 
 //Create a container object called the `stage`
 var stage = new PIXI.Container();
-//Tell the `renderer` to `render` the `stage`
+//---------------loader to load the sprite sheet -----------
+  PIXI.loader
+     .add('spritSheet1' , "./assets/ship_sprite.png")
+    .load(setup)
 
-function drawStars(){
-  for(var i = 0 ; i < 1500 ; i++){
-    var x = Math.round(Math.random() * window.innerWidth)
-    var y = Math.round(Math.random() * window.innerHeight)
-    var rad = Math.ceil(Math.random() * 2)
-    var alpha = Math.min(Math.random() +  0.25 , 1)
+    var spaceShip , idle2 , galaxy , galaxy2 , infos , sideStars;
 
-    var star = new PIXI.Graphics();
-    star.beginFill( 0xFFFFFF , alpha)
-    star.drawCircle(x , y , rad)
-    star.endFill()
-    stage.addChild(star)
+    function setup(){
+//---------------------- spaceShip animation -----------------//
+
+    var rect = new PIXI.Rectangle(0,0,1000,300 );
+    var texture = PIXI.loader.resources["spritSheet1"].texture;
+    texture.frame = rect;
+      spaceShip = new PIXI.Sprite(texture);
+      spaceShip.anchor.set(0.5);
+
+    // move the sprite to the center of the screen
+    spaceShip.x = renderer.width / 2 - 600;
+    spaceShip.y = renderer.height / 2 + 240;
+    spaceShip.width = 400;
+    spaceShip.height = 100;
+
+
+    idle2 = setInterval(function(){
+            if(rect.x >= 2000 ) {
+              rect.x = 0;
+            }
+            else{
+            spaceShip.texture.frame = rect;
+            rect.x += 1000;
+            }
+          }, 100);
+//---------------------------------------------------
+//---------- adding 1st galaxy img ------------------//
+       galaxy = new PIXI.Sprite.fromImage('./assets/galaxy.png')
+       galaxy.x = renderer.width / 2 - 900;
+       galaxy.y = renderer.height / 2 - 400;
+       galaxy.width = 500;
+       galaxy.height = 300;
+//------------------------------------------------------
+//------------------adding 2nd galaxy img --------------
+      galaxy2 = new PIXI.Sprite.fromImage('./assets/galaxy2.png')
+      galaxy2.anchor.set(0.5)
+      galaxy2.x = renderer.width / 2 + 700;
+      galaxy2.y = renderer.height / 2 + 300;
+      galaxy2.width = 400;
+      galaxy2.height = 200;
+
+//------------------------------------------------------
+
+//------------- adding personal-infos img --------------
+      infos = new PIXI.Sprite.fromImage('./assets/infos.png')
+      infos.anchor.set(0.5)
+      infos.x = renderer.width / 2 + 500;
+      infos.y = renderer.height / 2 ;
+      infos.width = 1000;
+      infos.height = 500;
+//------------------------------------------------------
+//-----------------adding stars to the left of the page
+      sideStars = new PIXI.Sprite.fromImage('./assets/stars.png')
+      sideStars.anchor.set(0)
+      sideStars.x = -100
+      sideStars.y = renderer.height / 2 - 150;
+      sideStars.width = 900;
+      sideStars.height = 800;
+//-------------------------------------------------------
+//-----------------moveing infos function ---------------
+        window.onkeydown = function(e) {
+        var key = e.keyCode ? e.keyCode : e.which;
+
+        if (key == 39) {
+           infos.x -= 10;
+           spaceShip.x += 4;
+           galaxy.x += 0.1;
+           galaxy2.x -= 0.1;
+           star.x += 0.41
+
+        }
+        else if (key == 37) {
+           infos.x += 10;
+           spaceShip.x -= 4;
+           galaxy.x -= 0.1;
+           galaxy2.x += 0.1;
+           star.x -= 0.41
+        }
+        }
+
+
+//-------------------------------------------------------
+
+
+
+
+
+
+
+      stage.addChild( sideStars , galaxy , galaxy2 , spaceShip , infos)
 
   }
-}
-drawStars()
-renderer.render(stage);
+  function animationLoop(){
+
+    requestAnimationFrame(animationLoop);
+
+      galaxy2.rotation -= 0.001
+    //Tell the `renderer` to `render` the `stage`
+    renderer.render(stage);
+
+  }
+
+
+  var star = new PIXI.Graphics();
+
+  for(var i = 0 ; i < 700 ; i++){
+    var x = Math.round(Math.random() * renderer.view.width );
+    var y = Math.round(Math.random() * renderer.view.height );
+    var rad = Math.ceil(Math.random() * 2);
+    var alpha = Math.min(Math.random() +  0.25 , 1);
+
+    star.beginFill( 0xFFFFFF , alpha);
+    star.drawCircle(x , y , rad);
+    star.endFill();
+  }
+
+  stage.addChild(star )
+animationLoop()
